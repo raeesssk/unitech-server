57372df52a3c24cc6f9ad46ac9dc419c69fac0de
zakir shaikh <zakrshkh07@gmail.com>
2018-07-30 19:59:14 +0530

    quotation updates

--- routes/quotation.js
+++ routes/quotation.js
@@ -34,7 +34,7 @@ router.post('/add', oauth.authorise(), (req, res, next) => {
   const results = [];
   const quotation=req.body.quotation;
   const purchaseMultipleData=req.body.purchaseMultipleData;
-  console.log(purchaseMultipleData);
+  const machineDetails=req.body.machineDetails
   pool.connect(function(err, client, done){
     if(err) {
       done();
@@ -44,16 +44,21 @@ router.post('/add', oauth.authorise(), (req, res, next) => {
     }
 
     var singleInsert = 'INSERT INTO quatation_master(qm_design_no, qm_quotation_no, qm_cm_id, qm_date, qm_ref_no, qm_status) values($1,$2,$3,$4,$5,0) RETURNING *',
-        params = [quotation.qm_design_no.dm_design_no,quotation.qm_quotation_no,quotation.qm_cm_id.cm_id,quotation.qm_date,quotation.qm_ref_no];
+        params = [quotation.qm_design_no.dm_design_no,quotation.qm_quotation_no,quotation.qm_design_no.dm_cm_id,quotation.qm_design_no.dm_dely_date,quotation.qm_ref_no];
     client.query(singleInsert, params, function (error, result) {
       
         results.push(result.rows[0]); // Will contain your inserted rows
+
         purchaseMultipleData.forEach(function(product, index) {
-        client.query('INSERT INTO quotation_product_master(qtm_part_no, qtm_part_name, qtm_qty, qtm_cost, qtm_total, qtm_qm_id, qtm_status)VALUES ($1, $2, $3, $4, $5, $6,  0)',
-          [product.qtm_part_no,product.qtm_part_name,product.qtm_qty,product.qtm_cost,product.qtm_total,result.rows[0].qm_id]);
-        
-          
-      });
+        client.query('INSERT INTO quotation_product_master(qpm_part_np, qpm_part_name, qpm_qty, qpm_qm_id, qpm_status)VALUES ($1, $2, $3, $4,  0)',
+          [product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].qm_id]);
+        });
+
+        machineDetails.forEach(function(value,key){
+          client.query("insert into quotation_product_machine_master( qpmm_mm_id, qpmm_qm_id, qpmm_mm_hr, qpmm_status)VALUES ($1, $2, $3, 0)",
+            [value.qpmm_mm_id.mm_id, result.rows[0].qm_id, value.qpmm_mm_hr]);
+          client.query("update quatation_master set qm_qpmm_id=$1 where qm_id=$2",[value.qpmm_mm_id.mm_id,result.rows[0].qm_id]);
+        });
 
         done();
         return res.json(results);
@@ -74,7 +79,7 @@ router.get('/:quotationId', oauth.authorise(), (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM quatation_master qm inner join customer_master cm on qm.qm_cm_id=cm.cm_id left outer join quotation_product_master qtm on qtm.qtm_qm_id=qm.qm_id left outer join design_product_master dtm on qtm.qtm_dtm_id=dtm.dtm_id where qm_id=$1',[id]);
+    const query = client.query('SELECT * FROM quatation_master qm inner join customer_master cm on qm.qm_cm_id=cm.cm_id left outer join quotation_product_master qpm on qpm.qpm_qm_id=qm.qm_id where qm_id=$1',[id]);
     query.on('row', (row) => {
       results.push(row);
     });
@@ -98,7 +103,31 @@ router.get('/product/:quotationId', oauth.authorise(), (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM quotation_product_master qpm left outer join quatation_master qm on qtm.qtm_qm_id=qm.qm_id where qm_id=$1',[id]);
+    const query = client.query('SELECT * FROM quotation_product_master qpm left outer join quatation_master qm on qpm.qpm_qm_id=qm.qm_id where qm_id=$1',[id]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+  done(err);
+  });
+});
+
+router.get('/machine/:quotationId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.quotationId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM quotation_product_machine_master qpmm left outer join quatation_master qm on qpmm.qpmm_qm_id=qm.qm_id left outer join machine_master mm on qpmm.qpmm_mm_id=mm.mm_id where qm_id=$1',[id]);
     query.on('row', (row) => {
       results.push(row);
     });
@@ -301,7 +330,31 @@ router.get('/view/:quotationId', oauth.authorise(), (req, res, next) => {
       console.log("the error is"+err);
       return res.status(500).json({success: false, data: err});
     }
-    const query = client.query("SELECT * FROM quotation_product_master qtm inner join quatation_master qm on qtm.qtm_qm_id=qm.qm_id where qm_id=$1",[id]);
+    const query = client.query("SELECT * FROM quotation_product_master qpm inner join quatation_master qm on qpm.qpm_qm_id=qm.qm_id where qm_id=$1",[id]);
+    query.on('row', (row) => {
+      results.push(row);
+
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+  done(err);
+  });
+});
+
+router.get('/views/:designId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id=req.params.designId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    const query = client.query("SELECT * FROM design_master dm LEFT OUTER JOIN customer_master cm on dm.dm_cm_id=cm.cm_id LEFT OUTER JOIN design_product_master dtm on dtm.dtm_dm_id=dm.dm_id  where dm_id=$1",[id]);
     query.on('row', (row) => {
       results.push(row);
 
