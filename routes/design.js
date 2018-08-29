d572edbc1352c1514d917b31c186a7e06417cac8
Raees Shaikh <raees@3commastechnologies.com>
2018-08-29 18:25:41 +0530

    bug fix in edit service design

--- routes/design.js
+++ routes/design.js
@@ -199,13 +199,13 @@ router.post('/edit/:designId', oauth.authorise(), (req, res, next) => {
         });
 
         oldDetails.forEach(function(product, index) {
-          client.query('update design_product_master set dtm_part_no=$1, dtm_part_name=$2, dtm_qty=$3, dtm_dm_id=$4, dtm_updated_at=now() where dtm_id=$5',[product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].dm_id,product.dtm_id]);
+          client.query('update design_product_master set dtm_part_no=$1, dtm_part_name=$2, dtm_qty=$3, dtm_dm_id=$4 where dtm_id=$5',[product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].dm_id,product.dtm_id]);
         });
 
         personalDetails.forEach(function(product, index) {
         client.query('INSERT INTO design_product_master(dtm_part_no, dtm_part_name, dtm_qty, dtm_dm_id, dtm_status)VALUES ($1, $2, $3, $4,  0)',
           [product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].dm_id]);
-        //client.query('update design_product_master set dtm_part_no=$1, dtm_part_name=$2, dtm_qty=$3, dtm_updated_at=now() where dtm_id=$4',[product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].dm_id]);
+        //client.query('update design_product_master set dtm_part_no=$1, dtm_part_name=$2, dtm_qty=$3 where dtm_id=$4',[product.dtm_part_no,product.dtm_part_name,product.dtm_qty,result.rows[0].dm_id]);
         });
 
         removeImagesDetails.forEach(function(product, index) {
