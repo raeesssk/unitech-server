8647f533ff8d94c73b87fa7191f348894808e050
Raees Shaikh <raees@3commastechnologies.com>
2018-09-03 15:31:47 +0530

    Quotation update

--- routes/quotation.js
+++ routes/quotation.js
@@ -54,9 +54,11 @@ router.get('/details/:quotationId', oauth.authorise(), (req, res, next) => {
     const strqry =  "select qm.qm_id, qm.qm_quotation_no, qm.qm_date, qm.qm_total_cost, qm.qm_net_cost, qm.qm_cgst_per, qm.qm_cgst_amount, qm.qm_sgst_per, qm.qm_sgst_amount, qm.qm_igst_per, qm.qm_igst_amount, qm.qm_transport, qm.qm_other_charges, qm.qm_discount, qm.qm_ref, qm.qm_comment, qm.qm_status, qm.qm_is_approve, qm.qm_created_at, qm.qm_updated_at, "+
                     "dm.dm_id, dm.dm_design_no, dm.dm_mft_date, dm.dm_dely_date, dm.dm_project_no, dm.dm_po_no, dm.dm_po_date, dm.dm_status, dm.dm_created_at, dm.dm_updated_at, "+
                     "cm_name||'-'||cm_address||'-'||cm_mobile as cm_search, cm.cm_id, cm.cm_name, cm.cm_mobile, cm.cm_address, cm.cm_state, cm.cm_city, cm.cm_pin_code, cm.cm_credit, cm.cm_debit, cm.cm_email, cm.cm_gst, cm.cm_opening_credit, cm.cm_opening_debit, cm.cm_status, cm.cm_created_at, cm.cm_updated_at, cm.cm_contact_person_name, cm.cm_contact_person_number, cm.cm_dept_name, "+
-                    "qpm.qpm_id, qpm.qpm_part_name, qpm.qpm_qty, qpm.qpm_part_no, qpm.qpm_total_cost "+
+                    "im_part_no||'-'||im_part_name ||' '||(im_quantity) as im_search, im.im_id, im.im_part_no, im.im_part_name, im.im_quantity, im.im_opening_quantity, im.im_price, im.im_mrp, im.im_status, im.im_created_at, im.im_updated_at, "+
+                    "qpm.qpm_id, qpm.qpm_qty, qpm.qpm_price, qpm.qpm_total_cost "+
                     "FROM quotation_product_master qpm "+
                     "inner join quotation_master qm on qpm.qpm_qm_id=qm.qm_id "+
+                    "inner join inventory_master qm on qpm.qpm_im_id=im.im_id "+
                     "inner join design_master dm on qm.qm_dm_id=dm.dm_id "+
                     "inner join customer_master cm on dm.dm_cm_id=cm.cm_id "+
                     "where qm.qm_id=$1";
@@ -89,10 +91,12 @@ router.get('/details/machine/:quotationId', oauth.authorise(), (req, res, next) 
                     "dm.dm_id, dm.dm_design_no, dm.dm_mft_date, dm.dm_dely_date, dm.dm_project_no, dm.dm_po_no, dm.dm_po_date, dm.dm_status, dm.dm_created_at, dm.dm_updated_at, "+
                     "cm_name||'-'||cm_address||'-'||cm_mobile as cm_search, cm.cm_id, cm.cm_name, cm.cm_mobile, cm.cm_address, cm.cm_state, cm.cm_city, cm.cm_pin_code, cm.cm_credit, cm.cm_debit, cm.cm_email, cm.cm_gst, cm.cm_opening_credit, cm.cm_opening_debit, cm.cm_status, cm.cm_created_at, cm.cm_updated_at, cm.cm_contact_person_name, cm.cm_contact_person_number, cm.cm_dept_name, "+
                     "mm_name||'-'||mm_price as mm_search, mm.mm_id, mm.mm_name, mm.mm_price, "+
-                    "qpm.qpm_id, qpm.qpm_part_name, qpm.qpm_qty, qpm.qpm_part_no, qpm.qpm_total_cost, "+
+                    "im_part_no||'-'||im_part_name ||' '||(im_quantity) as im_search, im.im_id, im.im_part_no, im.im_part_name, im.im_quantity, im.im_opening_quantity, im.im_price, im.im_mrp, im.im_status, im.im_created_at, im.im_updated_at, "+
+                    "qpm.qpm_id, qpm.qpm_qty, qpm.qpm_price, qpm.qpm_total_cost, "+
                     "qpmm.qpmm_id, qpmm.qpmm_total_cost, qpmm.qpmm_mm_hr "+
                     "FROM quotation_product_machine_master qpmm "+
                     "inner join quotation_product_master qpm on qpmm.qpmm_qpm_id=qpm.qpm_id "+
+                    "inner join inventory_master qm on qpm.qpm_im_id=im.im_id "+
                     "inner join machine_master mm on qpmm.qpmm_mm_id=mm.mm_id "+
                     "inner join quotation_master qm on qpm.qpm_qm_id=qm.qm_id "+
                     "inner join design_master dm on qm.qm_dm_id=dm.dm_id "+
@@ -135,8 +139,8 @@ router.post('/add', oauth.authorise(), (req, res, next) => {
 
         purchaseMultipleData.forEach(function(product, index) {
 
-          var singleInsertPro = 'INSERT INTO quotation_product_master(qpm_qm_id, qpm_part_name, qpm_qty, qpm_part_no, qpm_total_cost)VALUES ($1, $2, $3, $4, $5) RETURNING *',
-          paramsPro = [result.rows[0].qm_id,product.dtm_part_name,product.dtm_qty,product.dtm_part_no,product.dtm_total_cost];
+          var singleInsertPro = 'INSERT INTO quotation_product_master(qpm_qm_id, qpm_im_id, qpm_qty, qpm_price, qpm_total_cost)VALUES ($1, $2, $3, $4, $5) RETURNING *',
+          paramsPro = [result.rows[0].qm_id,product.im_id,product.dtm_qty,product.qpm_price,product.dtm_total_cost];
           
           client.query(singleInsertPro, paramsPro, function (errorPro, resultPro) {
 
@@ -183,7 +187,7 @@ router.post('/edit/:quotationId', oauth.authorise(), (req, res, next) => {
             var maclist = product.newMachineDetails;
             var remmaclist = product.removeMachineDetails;
 
-            client.query("update quotation_product_master set qpm_total_cost=$1 where qpm_id = $2",[product.qpm_total_cost,product.qpm_id]);
+            client.query("update quotation_product_master set qpm_price=$1, qpm_total_cost=$2 where qpm_id = $3",[product.qpm_price,product.qpm_total_cost,product.qpm_id]);
 
             remmaclist.forEach(function(value,key){
               client.query("delete from quotation_product_machine_master where qpmm_id = $1",[value.qpmm_id]);
