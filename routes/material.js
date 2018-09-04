7ad2b32df72e510b37ad305eede8b3d752bbaef7
Raees Shaikh <raees@3commastechnologies.com>
2018-09-04 23:12:36 +0530

    material service

---
+++ routes/material.js
@@ -0,0 +1,235 @@
+var express = require('express');
+var router = express.Router();
+var oauth = require('../oauth/index');
+var pg = require('pg');
+var path = require('path');
+var config = require('../config.js');
+
+var pool = new pg.Pool(config);
+
+router.get('/:mtmId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.mtmId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    const strqry =  "SELECT mtm_name||'-'||mtm_price as mtm_search, mtm.mtm_id, mtm.mtm_name, mtm.mtm_density, mtm.mtm_price, mtm.mtm_status, mtm.mtm_created_at, mtm.mtm_updated_at "+
+                    "FROM material_master mtm "+
+                    "where mtm.mtm_status = 0 "+
+                    "and mtm.mtm_id = $1";
+
+    // SQL Query > Select Data
+    const query = client.query(strqry,[id]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+  done(err);
+  });
+});
+
+router.post('/checkname', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    const strqry =  "SELECT mtm_name||'-'||mtm_price as mtm_search, mtm.mtm_id, mtm.mtm_name, mtm.mtm_density, mtm.mtm_price, mtm.mtm_status, mtm.mtm_created_at, mtm.mtm_updated_at "+
+                    "FROM material_master mtm "+
+                    "where mtm.mtm_status = 0 "+
+                    "and LOWER(mtm.mtm_name) like LOWER($1)";
+
+    // SQL Query > Select Data
+    const query = client.query(strqry,[req.body.mtm_name]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+  done(err);
+  });
+});
+
+router.post('/add', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    var singleInsert = 'INSERT INTO material_master(mtm_name, mtm_density, mtm_price, mtm_status) values($1,$2,$3,0) RETURNING *',
+        params = [req.body.mtm_name,req.body.mtm_density,req.body.mtm_price]
+    client.query(singleInsert, params, function (error, result) {
+        results.push(result.rows[0]); // Will contain your inserted rows
+        done();
+        return res.json(results);
+    });
+
+  done(err);
+  });
+});
+
+router.post('/edit/:mtmId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.mtmId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    client.query('BEGIN;');
+
+    var singleInsert = 'UPDATE material_master SET mtm_name=$1, mtm_density=$2, mtm_price=$3, mtm_updated_at=now() where mtm_id=$4 RETURNING *',
+        params = [req.body.mtm_name,req.body.mtm_density,req.body.mtm_price,id]
+    client.query(singleInsert, params, function (error, result) {
+        results.push(result.rows[0]); // Will contain your inserted rows
+        done();
+        client.query('COMMIT;');
+        return res.json(results);
+    });
+
+  done(err);
+  });
+});
+
+router.post('/delete/:mtmId', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  const id = req.params.mtmId;
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    client.query('BEGIN;');
+
+    var singleInsert = 'UPDATE material_master SET mtm_status=1, mtm_updated_at=now() WHERE mtm_id=($1) RETURNING *',
+        params = [id]
+    client.query(singleInsert, params, function (error, result) {
+        results.push(result.rows[0]); // Will contain your inserted rows
+        done();
+        client.query('COMMIT;');
+        return res.json(results);
+    });
+
+  done(err);
+  });
+});
+
+router.post('/material/total', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    const str = req.body.search+"%";
+
+    const strqry =  "SELECT count(mtm.mtm_id) as total "+
+                    "from material_master mtm "+
+                    "where mtm.mtm_status=0 "+
+                    "and LOWER(mtm_name||''||mtm_price ) LIKE LOWER($1);";
+
+    const query = client.query(strqry,[str]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+    done(err);
+  });
+});
+
+router.post('/material/limit', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    const str = req.body.search+"%";
+    // SQL Query > Select Data
+
+    const strqry =  "SELECT mtm_name||'-'||mtm_price as mtm_search, mtm.mtm_id, mtm.mtm_name, mtm.mtm_density, mtm.mtm_price, mtm.mtm_status, mtm.mtm_created_at, mtm.mtm_updated_at "+
+                    "FROM material_master mtm "+
+                    "where mtm.mtm_status = 0 "+
+                    "and LOWER(mtm_name||''||mtm_price ) LIKE LOWER($1) "+
+                    "order by mtm.mtm_id desc LIMIT $2 OFFSET $3";
+
+    const query = client.query(strqry,[str, req.body.number, req.body.begin]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+    done(err);
+  });
+});
+
+router.post('/typeahead/search', oauth.authorise(), (req, res, next) => {
+  const results = [];
+  pool.connect(function(err, client, done){
+    if(err) {
+      done();
+      // pg.end();
+      console.log("the error is"+err);
+      return res.status(500).json({success: false, data: err});
+    }
+    const str = req.body.search+"%";
+    // SQL Query > Select Data
+
+    const strqry =  "SELECT mtm_name||'-'||mtm_price as mtm_search, mtm.mtm_id, mtm.mtm_name, mtm.mtm_density, mtm.mtm_price, mtm.mtm_status, mtm.mtm_created_at, mtm.mtm_updated_at "+
+                    "FROM material_master mtm "+
+                    "where mtm.mtm_status = 0 "+
+                    "and LOWER(mtm_name||' '||mtm_price ) LIKE LOWER($1) "+
+                    "order by mtm.mtm_id desc LIMIT 16";
+
+    const query = client.query(strqry,[str]);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      // pg.end();
+      return res.json(results);
+    });
+    done(err);
+  });
+});
+
+
+module.exports = router;
